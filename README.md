271bfdf043ded00d8117cd70826924aa28424a41
cfoulk <charlesfoulk@csus.edu>
2022-01-06 21:04:52 -0800

    made some progress, still need to fix 1 start/end

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Maze is a website that visualizes graph pathfinding algorithms.
   - [x] Insert new start/end points
   - [x] Insert walls 
   - [ ] Insertion alters data within the grid data structure
-  - [ ] Buttons created for different states of adding to the grid (start/end/walls)
+  - [x] Buttons created for different states of adding to the grid (start/end/walls)
 - [ ] Path finding algorithm implementation with BFS/DFS.
 - [ ] Maze creation with BFS/Recursive division algorithms.
 - [ ] Advanced animation with block being added to grid(from walls, graph traversals, etc.)
--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const wallsButton = document.querySelector(".walls-button");
 const startButton = document.querySelector(".start-button");
 const endButton = document.querySelector(".end-button");
 
+let startX = 10;
+let startY = 15;
+let endX = 40;
+let endY = 15;
+
 class Node {
   constructor(row, col, isStart, isEnd, isWall) {
     this.row = row;
@@ -14,6 +19,18 @@ class Node {
     this.isStart = isStart;
     this.isEnd = isEnd;
     this.isWall = isWall;
+  }
+
+  setWall(b) {
+    this.isWall = b;
+  }
+
+  setStart(b) {
+    this.isStart = b;
+  }
+
+  setEnd(b) {
+    this.isEnd = b;
   }
 }
 
@@ -42,19 +59,84 @@ function createCanvas() {
     const currRow = [];
     for (let j = 0; j < COLS; j++) {
       let pix = document.createElement("div");
-      pix.setAttribute("id", "node-" + i + "-" + j);
+      currRow.push(new Node(j, i, false, false, false));
+      pix.setAttribute("id", "node " + i + " " + j);
+      pix.setAttribute("data-x", i);
+      pix.setAttribute("data-y", j);
       pix.classList.add("cell");
       pix.style.position = "absolute";
       pix.style.left = j * PIXEL + "px";
       pix.style.top = i * PIXEL + "px";
       pix.style.width = PIXEL + "px";
       pix.style.height = PIXEL + "px";
+      if (j == startX && i == startY) {
+        pix.classList.add("start");
+        currRow[j].setStart(true);
+      }
+      if (j == endX && i == endY) {
+        pix.classList.add("end");
+        currRow[j].setEnd(true);
+      }
       canvas.appendChild(pix);
       //console.log(new Node(i, j, false, false, false));
-      currRow.push(new Node(i, j, false, false, false));
     }
-    //console.log(currRow.toString());
-    grid.push[currRow];
+    //console.log(grid[i].toString());
+    grid.push(currRow);
+  }
+}
+
+function addWall(x, y, event) {
+  if (event.target.classList != "cell wall") {
+    event.target.classList.remove("start");
+    event.target.classList.remove("end");
+    event.target.classList.add("wall");
+    grid[x][y].setWall(true);
+    grid[x][y].setStart(false);
+    grid[x][y].setEnd(false);
+    console.log(grid[x][y]);
+  } else {
+    event.target.classList.remove("wall");
+    grid[x][y].setWall(false);
+    console.log(grid[x][y]);
+  }
+}
+function addStart(x, y, event) {
+  if (event.target.classList != "cell start") {
+    let id = "node " + startX + " " + startY;
+    document.getElementById(id).classList.remove("start");
+    grid[startX][startY].setStart(false);
+    event.target.classList.remove("wall");
+    event.target.classList.remove("end");
+    event.target.classList.add("start");
+    console.log(grid[startX][startY]);
+    grid[x][y].setStart(true);
+    grid[x][y].setWall(false);
+    grid[x][y].setEnd(false);
+    startX = x;
+    startY = y;
+    console.log(grid[startX][startY]);
+  } else {
+    event.target.classList.remove("start");
+    grid[x][y].setStart(false);
+    console.log(grid[x][y]);
+  }
+}
+function addEnd(x, y, event) {
+  if (event.target.classList != "cell end") {
+    event.target.classList.remove("wall");
+    event.target.classList.remove("start");
+    event.target.classList.add("end");
+    grid[endX][endY].setEnd(false);
+    let oldEnd = document.getElementById("node" + x + " " + y);
+    //event.target.classList.remove("end");
+    grid[x][y].setEnd(true);
+    grid[x][y].setWall(false);
+    grid[x][y].setStart(false);
+    console.log(grid[x][y]);
+  } else {
+    event.target.classList.remove("end");
+    grid[x][y].setEnd(false);
+    console.log(grid[x][y]);
   }
 }
 
@@ -64,28 +146,18 @@ createCanvas();
 const nodeSelector = [...document.querySelectorAll(".cell")];
 for (const nodes of nodeSelector) {
   nodes.addEventListener("mousedown", (event) => {
-    console.log("lol " + event.target.id);
+    console.log("id: " + event.target.id);
+    console.log("x: " + event.target.dataset.x);
+    console.log("y: " + event.target.dataset.y);
 
     if (currentSetting == "WALL") {
-      if (event.target.classList != "cell wall") {
-        event.target.classList.add("wall");
-      } else {
-        event.target.classList.remove("wall");
-      }
+      addWall(event.target.dataset.x, event.target.dataset.y, event);
     }
     if (currentSetting == "START") {
-      if (event.target.classList != "cell start") {
-        event.target.classList.add("start");
-      } else {
-        event.target.classList.remove("start");
-      }
+      addStart(event.target.dataset.x, event.target.dataset.y, event);
     }
     if (currentSetting == "END") {
-      if (event.target.classList != "cell end") {
-        event.target.classList.add("end");
-      } else {
-        event.target.classList.remove("end");
-      }
+      addEnd(event.target.dataset.x, event.target.dataset.y, event);
     }
   });
 }
