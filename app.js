940e0fa8b4318e26b1f8bd84e25a882004bb912f
charlie foulk <charlesfoulk@csus.edu>
2023-03-26 14:25:15 -0700

    making logs more reasonable

--- app.js
+++ app.js
@@ -35,10 +35,8 @@ function createCanvas() {
         currRow[j].setEnd(true);
       }
       canvas.appendChild(pix);
-      // console.log(new Node(i, j, false, false, false));
     }
     grid.push(currRow);
-    // console.log(grid[i].toString());
   }
 }
 
@@ -50,73 +48,72 @@ function addWall(x, y, event) {
     grid[y][x].setWall(true);
     grid[y][x].setStart(false);
     grid[y][x].setEnd(false);
-    console.log(grid[y][x]);
+    // console.log(grid[y][x]);
   } else {
     event.target.classList.remove("wall");
     grid[y][x].setWall(false);
-    console.log(grid[y][x]);
+    // console.log(grid[y][x]);
   }
 }
 
 function addStart(x, y, event) {
   if (event.target.classList != "cell start") {
     let id = "node " + startX + " " + startY;
-    console.log(id);
+    // console.log(id);
     document.getElementById(id).classList.remove("start");
     grid[startY][startX].setStart(false);
     event.target.classList.remove("wall");
     event.target.classList.remove("end");
     event.target.classList.add("start");
-    console.log(grid[startY][startX]);
+    // console.log(grid[startY][startX]);
     grid[y][x].setStart(true);
     grid[y][x].setWall(false);
     grid[y][x].setEnd(false);
     startX = x;
     startY = y;
-    console.log(grid[startY][startX]);
+    // console.log(grid[startY][startX]);
   } else {
     event.target.classList.remove("start");
     grid[y][x].setStart(false);
-    console.log(grid[y][x]);
+    // console.log(grid[y][x]);
   }
 }
 
 function addEnd(x, y, event) {
   if (event.target.classList != "cell end") {
     let id = "node " + endX + " " + endY;
-    console.log(id);
+    // console.log(id);
 
     document.getElementById(id).classList.remove("end");
     grid[endY][endX].setEnd(false);
     event.target.classList.remove("wall");
     event.target.classList.remove("start");
     event.target.classList.add("end");
-    console.log(grid[endY][endX]);
+    // console.log(grid[endY][endX]);
     grid[y][x].setEnd(true);
     grid[y][x].setWall(false);
     grid[y][x].setStart(false);
     endX = x;
     endY = y;
-    console.log(grid[endY][endX]);
+    // console.log(grid[endY][endX]);
   } else {
     event.target.classList.remove("end");
     grid[y][x].setEnd(false);
-    console.log(grid[y][x]);
+    // console.log(grid[y][x]);
   }
 }
 
 createCanvas();
 
-console.log(JSON.stringify(grid[15][10]));
-console.log(JSON.stringify(grid[15][40]));
-console.log(JSON.stringify(grid[15][16]));
-
+// grid[15][10].toString();
+// grid[15][40].toString();
+// grid[15][16].toString();
 const nodeSelector = [...document.querySelectorAll(".cell")];
 for (const nodes of nodeSelector) {
   nodes.addEventListener("mousedown", (event) => {
-    console.log("id: " + event.target.id);
-    console.log("x: " + event.target.dataset.x);
-    console.log("y: " + event.target.dataset.y);
+    // console.log("id: " + event.target.id);
+    // console.log("x: " + event.target.dataset.x);
+    // console.log("y: " + event.target.dataset.y);
 
     if (currentSetting == "WALL") {
       addWall(event.target.dataset.x, event.target.dataset.y, event);
--- modules/node.js
+++ modules/node.js
@@ -1,25 +1,29 @@
 export default class Node {
-  Node(col, row, isStart, isEnd, isWall) {
-    this.col = col; //x
-    this.row = row; //y
-    this.isStart = isStart;
-    this.isEnd = isEnd;
-    this.isWall = isWall;
-    this.visted = false;
-    this.gScore = Infinity;
-    this.fScore = Infinity;
-    this.parent = null;
-  }
+    constructor(col, row, isStart, isEnd, isWall) {
+        this.col = col; //x
+        this.row = row; //y
+        this.isStart = isStart;
+        this.isEnd = isEnd;
+        this.isWall = isWall;
+        this.visted = false;
+        this.gScore = Infinity;
+        this.fScore = Infinity;
+        this.parent = null;
+    }
 
-  setWall(b) {
-    this.isWall = b;
-  }
+    setWall(b) {
+        this.isWall = b;
+    }
 
-  setStart(b) {
-    this.isStart = b;
-  }
+    setStart(b) {
+        this.isStart = b;
+    }
 
-  setEnd(b) {
-    this.isEnd = b;
-  }
+    setEnd(b) {
+        this.isEnd = b;
+    }
+
+    toString() {
+        console.log("col: " + this.col + "\nrow: " + this.row + "\nstart: " + this.isStart + "\nend: " + this.isEnd + "\nwall: " + this.isWall)
+    }
 }
